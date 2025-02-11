e555d0f5c9ef033965338fffae06ee1780ffd6f5
riyuk12 <72291800+riyuk12@users.noreply.github.com>
2025-02-11 15:07:36 +0530

    Update script.js

--- prep/htmlcards/script.js
+++ prep/htmlcards/script.js
@@ -1,2 +1,16 @@
-const suits = ['♠', '♥', '♦', '♣'];
-const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const generateCard=()=>{
+    const suits = ['♠', '♥', '♦', '♣'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+    const top=document.getElementById("top-suit")
+    const card=document.getElementById("card-value")
+    const bot=document.getElementById("bottom-suit")
+
+    let randomSuit=Math.floor(Math.random()*suits.length)
+    let randomvalue=Math.floor(Math.random()*values.length)
+
+    top.innerText=suits[randomSuit]
+    bot.innerText=suits[randomSuit]
+    card.innerText=values[randomvalue]
+
+}
